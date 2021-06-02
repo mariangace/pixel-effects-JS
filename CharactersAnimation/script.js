ceb701bce5f573d4dd6e24b385f5efa15128d0f6
mariangace <mariangace@gmail.com>
2021-06-01 21:26:24 -0500

    mantain characters in canvas area

--- CharactersAnimation/script.js
+++ CharactersAnimation/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CAVANS_HEIGHT = canvas.height = 1000;
 
-let numberOfEnemies = 50;
+let numberOfEnemies = 10;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -12,19 +12,20 @@ class Enemy {
   constructor() {
     this.image = new Image();
     this.image.src = './assets/enemy1.png';
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
+
+    //this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0; // for sprites
     this.flapSpeed = Math.floor(Math.random() * 3 + 1); //every enemy will show a different velocity
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 5 - 2.5;
+    this.y += Math.random() * 5 - 2.5;
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
